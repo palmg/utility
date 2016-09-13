93bd9a475a0519ebfc0c6b88270fd667db21ab66
chkui <chenkui@chkui.com>
2016-09-13 11:04:11 +0800

    add new util

--- src/main/java/com/palmg/utility/annotation/Fluently.java
+++ src/main/java/com/palmg/utility/annotation/Fluently.java
@@ -1,5 +1,10 @@
 package com.palmg.utility.annotation;
 
+/**
+ * 标记流式编程接口，一定不会返回null
+ * @author chenkui
+ *
+ */
 public @interface Fluently {
 	
 }
---
+++ src/main/java/com/palmg/utility/annotation/Nullable.java
@@ -0,0 +1,10 @@
+package com.palmg.utility.annotation;
+
+/**
+ * 标记当前接口可能会返回null
+ * @author chenkui
+ *
+ */
+public @interface Nullable {
+
+}
